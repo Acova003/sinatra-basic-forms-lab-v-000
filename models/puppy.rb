d1d3dd62552820237c2c78eb115a44c1ed08fa0e
Amee Covarrubias <ameecova@yahoo.com>
2019-06-08 21:18:55 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,6 @@ class Puppy
   attr_reader :name, :breed, :months_old
   
   def initialize(name, breed, months_old)
-    @name = name 
-    @breed = breed 
-    @months_old = months_old
+    
   end
 end 
